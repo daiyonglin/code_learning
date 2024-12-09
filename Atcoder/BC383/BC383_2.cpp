b79f1541b381cd4ef5774bca9f842030963e8334
daiyonglin <3084215866@qq.com>
2024-12-09 19:06:37 +0800

    clean

---
+++ Atcoder/BC383/BC383_2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int h,w,d;
+    cin>>h>>w>>d;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    solve();
+    return 0;
+}
